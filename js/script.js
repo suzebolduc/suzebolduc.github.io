50d1ce319036905f45974d4ac215a8e1ac78d057
suzebolduc <49281449+suzebolduc@users.noreply.github.com>
2019-04-08 11:01:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function(){
   $('div').click(function(){
     $(this).hide();
   });
+  $('img').click(function(){
+    $('img').css({border-radius': '100px'});
+  }):
+});
